332e749ddda3a915945e7e664f191c7a93a4bbcc
Rajwol Chapagain <rchapagain@cobalt08.icecube.wisc.edu>
2024-06-02 20:30:36 -0500

    Save info about bad rate days to disk

--- rate_check.py
+++ rate_check.py
@@ -6,6 +6,33 @@ import matplotlib.pyplot as plt
 
 import directories as stab
 
+def save_info(day, cfg, f):
+    f.write(f'{cfg},')
+    f.write(f'{day}\n')
+
+    with open(f'/data/user/rchapagain/stability/{cfg}_summary.txt', 'r') as summary:
+        lines = summary.readlines()
+        recording = False
+
+        # Go through all lines in the appropriate summary file
+        for line in lines:
+            if recording:
+                f.write(',,')
+                items = line.split('-')
+
+                for item in items:
+                    f.write(item.strip() + ',')     
+
+                f.write('\n')
+
+            # Start recording data when the line has the target day
+            if day in line:
+                recording = True
+                
+            # Stop recording data if the line has the word fits in it
+            if 'fits' in line:
+                recording = False
+
 if __name__ == "__main__":
 
     # Import default project-specific paths
@@ -34,75 +61,79 @@ if __name__ == "__main__":
     with open(args.rate_file, 'r') as f:
         rates = json.load(f)
 
-    # Produce rate plots for root and fits data
-    for ftype in rates.keys():
+    with open('bad_rates.csv', 'w') as f:
+        f.write('Configuration, Day, Runs\n')
+        # Produce rate plots for root and fits data
+        for ftype in rates.keys():
 
-        all_rates = []
-        all_dates = []
-        configs = sorted(rates[ftype].keys())
-        for cfg in configs:
+            all_rates = []
+            all_dates = []
+            configs = sorted(rates[ftype].keys())
 
-            cfg_rates = [r for day, r in rates[ftype][cfg].items()]
-            cfg_dates = [day for day, r in rates[ftype][cfg].items()]
-            all_rates += cfg_rates
-            all_dates += cfg_dates
+            for cfg in configs:
 
-            if args.verbose:
-                # Find and print days with bad rates
-                print(f'Bad rates for {cfg}:')
+                cfg_rates = [r for day, r in rates[ftype][cfg].items()]
+                cfg_dates = [day for day, r in rates[ftype][cfg].items()]
+                all_rates += cfg_rates
+                all_dates += cfg_dates
 
-                # Find comparison # from median of first 7 days
-                comp = np.median(cfg_rates[:7])
+                if args.verbose:
+                    # Find and print days with bad rates
+                    print(f'Bad rates for {cfg}:')
 
-                for i, (day, rate) in enumerate(rates[ftype][cfg].items()):
+                    # Find comparison # from median of first 7 days
+                    comp = np.median(cfg_rates[:7])
 
-                    if (rate >= (1+dr)*comp) or (rate <= (1-dr)*comp):
-                        print(f'  {day}')
-                        continue
+                    for i, (day, rate) in enumerate(rates[ftype][cfg].items()):
 
-                    # Include good rates into rolling average
-                    comp = (4*comp + rate) / 5
+                        if (rate >= (1+dr)*comp) or (rate <= (1-dr)*comp):
+                            print(f'  {day}')
+                            save_info(day, cfg, f)
+                            continue
 
-                print()
+                        # Include good rates into rolling average
+                        comp = (4*comp + rate) / 5
+
+                    print()
 
 
-            # Plot rate over time
-            fig, ax = plt.subplots()
-            ax.plot(cfg_rates)
+                # Plot rate over time
+                fig, ax = plt.subplots()
+                ax.plot(cfg_rates)
+                ax.set_ylim(1600, 3000)
+
+                # Adjust x-axis labels to show dates
+                fig.canvas.draw()
+                labels = [item.get_text() for item in ax.get_xticklabels()]
+                for i in range(1, len(labels)):
+                    try: labels[i] = cfg_dates[int(labels[i])]
+                    except IndexError:
+                        labels[i] = ''
+                ax.set_xticklabels(labels, rotation=45, ha='right')
+
+                # Save plot to png file
+                out = f'{args.outdir}/rates_{ftype}_{cfg}.png'
+                plt.savefig(out, bbox_inches="tight")
+                plt.close()
+
+            # Plot for all years
+            fig, ax = plt.subplots(figsize=(20,7))
+            ax.plot(all_rates)
             ax.set_ylim(1600, 3000)
 
             # Adjust x-axis labels to show dates
             fig.canvas.draw()
             labels = [item.get_text() for item in ax.get_xticklabels()]
             for i in range(1, len(labels)):
-                try: labels[i] = cfg_dates[int(labels[i])]
+                try: labels[i] = all_dates[int(labels[i])]
                 except IndexError:
                     labels[i] = ''
             ax.set_xticklabels(labels, rotation=45, ha='right')
 
-            # Save plot to png file
-            out = f'{args.outdir}/rates_{ftype}_{cfg}.png'
+
+            out = f'{args.outdir}/rates_{ftype}_IC86.png'
             plt.savefig(out, bbox_inches="tight")
             plt.close()
-
-        # Plot for all years
-        fig, ax = plt.subplots(figsize=(20,7))
-        ax.plot(all_rates)
-        ax.set_ylim(1600, 3000)
-
-        # Adjust x-axis labels to show dates
-        fig.canvas.draw()
-        labels = [item.get_text() for item in ax.get_xticklabels()]
-        for i in range(1, len(labels)):
-            try: labels[i] = all_dates[int(labels[i])]
-            except IndexError:
-                labels[i] = ''
-        ax.set_xticklabels(labels, rotation=45, ha='right')
-
-
-        out = f'{args.outdir}/rates_{ftype}_IC86.png'
-        plt.savefig(out, bbox_inches="tight")
-        plt.close()
 
     print(f'Finished. Plots saved to {args.outdir}')
 
